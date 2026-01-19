8dd3b1c87e473137eba5d51f298491e56938fbf5
rrypratikshagaikwad <rrypratikshagaikwad@gmail.com>
2026-01-19 12:05:22 +0530

    deploye db changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,28 @@
+// const mysql = require("mysql2/promise");
+
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "attendance",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = db;
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "attendance",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : false
 });
 
 module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use("/export", exportRoutes);
 require("./cron/autoAbsent");
 
 
+const PORT = process.env.PORT || 5000;
 app.listen(5000, () => {
     console.log("Backend running on http://localhost:5000");
 });
